8ef799431adbb17166f9a02992ceb62e4dc923d9
LucasUTNFRD <ldelgado@frd.utn.edu.ar>
2025-12-20 18:06:49 -0300

    add day 11

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::{fs::read_to_string, time::Instant};
 use aoc_rs::year2025;
 
 fn main() {
-    let data = read_to_string("inputs/year2025/day10.txt").unwrap();
+    let data = read_to_string("inputs/year2025/day11.txt").unwrap();
 
-    let data = year2025::day10::parse(&data);
+    // let data = year2025::day11::parse(&data);
 
     let start = Instant::now();
-    let solution_part_1 = year2025::day10::part_1(&data);
+    let solution_part_1 = year2025::day11::part_1(&data);
     let duration_part_1 = start.elapsed();
     println!(
         "Day 10 - Part 1 solution {} - elapsed {:#?}",
@@ -17,7 +17,7 @@ fn main() {
 
     // let input = year2025::day06::parse_part2(&data);
     let start = Instant::now();
-    let solution_part_2 = year2025::day10::part_2(&data);
+    let solution_part_2 = year2025::day11::part_2(&data);
     let duration_part_2 = start.elapsed();
     println!(
         "Day 10 - Part 2 solution {} - elapsed {:#?}",
---
+++ src/year2025/day11.rs
@@ -0,0 +1,95 @@
+use rustc_hash::FxHashMap as HashMap;
+
+const STARTING_DEVICE: &str = "you";
+const GOAL: &str = "out";
+
+pub fn part_1(input: &str) -> u128 {
+    let device_kv: HashMap<&str, Vec<&str>> = {
+        let mut device_kv = HashMap::default();
+
+        for l in input.lines() {
+            let (k, value_str) = l.split_once(": ").unwrap();
+            let values = value_str.split_whitespace().collect();
+
+            device_kv.insert(k, values);
+        }
+
+        device_kv
+    };
+
+    let mut memo: HashMap<&str, u128> = HashMap::default();
+
+    dfs(STARTING_DEVICE, &device_kv, &mut memo, GOAL)
+}
+
+pub fn part_2(input: &str) -> u128 {
+    let device_kv: HashMap<&str, Vec<&str>> = {
+        let mut device_kv = HashMap::default();
+
+        for l in input.lines() {
+            let (k, value_str) = l.split_once(": ").unwrap();
+            let values = value_str.split_whitespace().collect();
+
+            device_kv.insert(k, values);
+        }
+
+        device_kv
+    };
+
+    // SEQUENCE 1: svr -> fft -> dac -> GOAL
+    let seq_1 = {
+        let mut m1 = HashMap::default();
+        let s1 = dfs("svr", &device_kv, &mut m1, "fft");
+
+        let mut m2 = HashMap::default();
+        let s2 = dfs("fft", &device_kv, &mut m2, "dac");
+
+        let mut m3 = HashMap::default();
+        let s3 = dfs("dac", &device_kv, &mut m3, GOAL);
+
+        s1 * s2 * s3
+    };
+
+    // SEQUENCE 2: svr -> dac -> fft -> GOAL
+    let seq_2 = {
+        let mut m1 = HashMap::default();
+        let s1 = dfs("svr", &device_kv, &mut m1, "dac");
+
+        let mut m2 = HashMap::default();
+        let s2 = dfs("dac", &device_kv, &mut m2, "fft");
+
+        let mut m3 = HashMap::default();
+        let s3 = dfs("fft", &device_kv, &mut m3, GOAL);
+
+        s1 * s2 * s3
+    };
+
+    seq_2 + seq_1
+}
+
+fn dfs<'a>(
+    curr: &'a str,
+    graph: &HashMap<&'a str, Vec<&'a str>>,
+    memo: &mut HashMap<&'a str, u128>,
+    goal: &str,
+) -> u128 {
+    if let Some(&count) = memo.get(curr) {
+        return count;
+    }
+
+    if curr == goal {
+        return 1;
+    }
+
+    let mut total_paths = 0;
+
+    if let Some(neighbors) = graph.get(curr) {
+        for next in neighbors {
+            total_paths += dfs(next, graph, memo, goal);
+        }
+    }
+
+    // Store in Cache and Return
+    memo.insert(curr, total_paths);
+    total_paths
+}
